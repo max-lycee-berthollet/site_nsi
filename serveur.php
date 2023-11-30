605a24f7ef0fbec4cf7ffb4b8e88640642e083d9
max-lycee-berthollet <146076358+max-lycee-berthollet@users.noreply.github.com>
2023-11-30 11:42:26 +0100

    Update serveur.php

--- serveur.php
+++ serveur.php
@@ -12,21 +12,21 @@
   <p>
     <?php
 	$host="localhost";
-	$username="id21586644_admin";
+	$usernom="id21586644_admin";
 	$password="Ploup12345#";
-	$dbname="id21586644_ploup";
+	$dbnom="id21586644_ploup";
 	
-	$conn=mysqli_connect($host, $username, $password, $dbname);
+	$conn=mysqli_connect($host, $usernom, $password, $dbnom);
 	if(mysqli_connect_errno())
 		echo "connection could not established...".mysqli_connect_error();
 	else
 	{
 		echo "Successfully connected... <br>";
-		if (isset($_GET['name']) && isset($_GET['mail'])){	
-			$name = $_GET['name'];
+		if (isset($_GET['nom']) && isset($_GET['mail'])){	
+			$nom = $_GET['nom'];
 			$mail = $_GET['mail'];
 			$query = "INSERT INTO classement VALUES (?, ?)";
-			$result = mysqli_execute_query($conn, $query, [$name, $mail]);
+			$result = mysqli_execute_query($conn, $query, [$nom, $mail]);
 		}
 		$query = "SELECT * FROM classement ORDER BY mail DESC";
 		$result = mysqli_execute_query($conn, $query);
@@ -41,7 +41,7 @@
 		<tbody>
 		<?php foreach ($result as $row){ ?>
 		    <tr>
-			    <td><?php echo $row['name'];?></td>
+			    <td><?php echo $row['nom'];?></td>
 				<td><?php echo $row['mail'];?></td>
 			</tr>
 		<?php } ?>
