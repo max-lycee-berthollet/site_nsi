e4523bd58944902c13e89308157028bbdac7a39f
Ellanodem <146076419+Ellanodem@users.noreply.github.com>
2023-12-07 11:48:44 +0100

    Update Javascript.js

--- Javascript.js
+++ Javascript.js
@@ -9,22 +9,3 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // Lancer l'animation après un délai de 1 seconde
   setTimeout(animerTexte, 100);
-  document.addEventListener('DOMContentLoaded', function() {
-  const texte = "Pour commencer creer un compte en allant sur la page connection puis vous pourrez poster en ligne...";
-  const texteAnimation = document.getElementById('texteAnimation');
-
-  function animerTexte2(texte, index) {
-    texteAnimation.textContent += texte.charAt(index);
-    index++;
-
-    if (index < texte.length) {
-      setTimeout(function() {
-        animerTexte2(texte, index);
-      }, 50); // Délai entre l'affichage de chaque lettre (100 millisecondes)
-    }
-  }
-
-  // Lancer l'animation
-  animerTexte2(texte, 0);
-});
-});
