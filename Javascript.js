2d8de3c11898e1b00363357cac1f45d85dbe0ce1
Ellanodem <146076419+Ellanodem@users.noreply.github.com>
2023-12-07 11:09:53 +0100

    Create Javascript.js

---
+++ Javascript.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Récupération de l'élément texte d'accueil
+  const texteAccueil = document.getElementById('texteAccueil');
+
+  // Fonction pour animer l'apparition du texte
+  function animerTexte() {
+    texteAccueil.style.opacity = 1; // Affichage du texte
+  }
+
+  // Lancer l'animation après un délai de 1 seconde
+  setTimeout(animerTexte, 1000);
+});
