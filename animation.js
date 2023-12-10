fd984e1e475e6ae82512e5792c9a94739fc957f0
Ellanodem <146076419+Ellanodem@users.noreply.github.com>
2023-12-10 22:00:15 +0100

    Update animation.js

--- animation.js
+++ animation.js
@@ -19,7 +19,17 @@ window.onload = function() {
   }, 1100);
 
   button.addEventListener('click', () => {
-    // Add actions on button click
-    alert('Let the journey begin!');
-  });
+  title.style.transition = 'transform 1s';
+  text.style.transition = 'transform 1s';
+  button.style.transition = 'transform 1s';
+
+  title.style.transform = 'translateY(-100vh)';
+  text.style.transform = 'translateY(-100vh)';
+  button.style.transform = 'translateY(-100vh)';
+
+  setTimeout(() => {
+    // Changer l'URL pour rediriger vers le site donné (par exemple, https://www.example.com)
+    window.location.href = 'https://www.google.com/';
+  }, 1000); // Délai pour l'animation avant la redirection, ici 1000ms (1 seconde)
+});;
 };
