7c85dd5247c7555140e3b8e527b5671162a9da9b
max-lycee-berthollet <146076358+max-lycee-berthollet@users.noreply.github.com>
2023-12-11 21:11:53 +0100

    Update postes.php

--- postes.php
+++ postes.php
@@ -15,12 +15,24 @@ if (!isset($_SESSION['user_id'])) {
   <title>SocialNexa</title>
   <meta charset="utf-8">
   <link rel="stylesheet" href="style.css">
-  <script src="script.js"></script>
+
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  
+  <style>
+    /* CSS pour la classe .poste */
+    .poste {
+      margin-bottom: 10px;
+      padding: 5px;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+    }
+  </style>
+  
 </head>
 <body>
   <main id='tete'><section><img id='logo' src='image.png'></section><section><a href='index.html'>Accueil</a></section><section><a href='postes.php'>Postes</a></section><section><a href='publier_page.php'>Publier</a></section><section><a href='compte.php'>Compte</a></section></main>
 	<hr>
-	
+	<div id='messages'>
 	<?php
 	    $host = "localhost";
         $usernom = "id21586644_admin";
@@ -38,9 +50,9 @@ if (!isset($_SESSION['user_id'])) {
                 // Vérification de la préparation de la requête
                 if (mysqli_num_rows($result) > 0) {
                     while ($row = $result->fetch_assoc()){
-                        echo $row['nom'] ." ". $row['heure'] . "<br>"; 
+                        echo "<div class = 'poste'>" . '<b>'. $row['nom'] .'</b>'." ". $row['heure'] . "<br>"; 
                         echo $row['poste'] . "<br>";
-                        echo "<br>";
+                        echo "</div>" ;
                     
                     }
                     
@@ -56,6 +68,35 @@ if (!isset($_SESSION['user_id'])) {
         
 	    
 	?>
+	</div>
+	<script>
+    $(document).ready(function() {
+        function fetchData() {
+            $.ajax({
+                url: 'recuperer_messages.php',
+                type: 'GET',
+                dataType: 'json', // Définit le type de données attendu dans la réponse
+                success: function(data) {
+                    $('#messages').empty(); // Vide la div #messages
+                    $.each(data, function(index, message) {
+                        $('#messages').append('<div class = "poste"><div class="tete">' + '<b>' + message.nom_utilisateur + '</b>' + " " + message.heure + '</div>' + '<div>' + message.poste + '</div>' + '</div>');
+                        
+                        
+                    });
+                },
+                error: function(xhr, status, error) {
+                    console.error(status + ": " + error); // En cas d'erreur, affichez la console
+                }
+            });
+        }
+
+        // Appel initial
+        fetchData();
+
+        // Rafraîchissement toutes les 5 secondes (5000 millisecondes)
+        setInterval(fetchData, 5000);
+    });
+</script>
 	
 	
 </body>
